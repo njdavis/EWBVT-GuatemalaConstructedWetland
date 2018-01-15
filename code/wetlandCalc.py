bf12a2612edf54d4dbd175d5f6667ba96e6c8dbc
Noah Davis <noah_davis@outlook.com>
2018-01-15 17:18:55 -0500

    wetlandCalc has error

--- code/wetlandCalc.py
+++ code/wetlandCalc.py
@@ -1,5 +1,6 @@
 """Program to calculate constructed wetland design"""
 import sys
+import math
 
 #importing class definitions
 from siteInfo import Site
@@ -9,8 +10,14 @@ class VolumetricProcessDesignModel:
 
 
     def __init__(self):
-        #From "Natural Wastewater Treatment Systems"
+        #From "Natural Wastewater Treatment Systems". First order removal model
         self.BOD_Const = {'K_20':0.678, 'theta':1.06}
+
+        self.effluentConcentration = 10
+
+        #initialize with reasonable value used in example, but should change this
+        self.avgDepth = 0.5
+
         #initialize to an average value from Reed book
         self.porosity = 0.8
 
@@ -19,9 +26,15 @@ class VolumetricProcessDesignModel:
     def K_T(self, T_W):
         return (self.BOD_Const['K_20']*self.BOD_Const['theta']**(T_W-20))
 
+    def treatmentArea(self, Q_A,influentConcentration, K_T):
+        return Q_A(math.log(influentConcentration/self.effluentConcentration)/(K_T*self.avgDepth*self.porosity))
 
-test = VolumetricProcessDesignModel()
-test.BOD_Const['K_20'] = 20
-print(test.K_T(18))
+
+
+#example of how to use class
+reedModel = VolumetricProcessDesignModel()
+CEFONMA = Site()
+#reedModel.BOD_Const['K_20'] = 20
+print(reedModel.treatmentArea(CEFONMA.flowRate,CEFONMA.waterQualityData['BOD'], reedModel.K_T(18)))
 
 
