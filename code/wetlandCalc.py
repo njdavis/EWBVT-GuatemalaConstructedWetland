2d6c8d74515f86c589b153108e506b0b282fe098
Noah Davis <noah_davis@outlook.com>
2018-01-15 15:56:53 -0500

    Made the Volumetric Process Design Model (Reed) its own class

--- code/wetlandCalc.py
+++ code/wetlandCalc.py
@@ -5,10 +5,15 @@ import sys
 from siteInfo import Site
 
 
-#Volumetric Design Equations
-def K_T(K_20,theta, T_W):
-    return (K_20*theta**(T_W-20))
+class VolumetricProcessDesignModel:
 
-test = K_T(0.678,1.06,18)
-print(test)
+
+    #Volumetric Design Equations
+    def K_T(self, K_20,theta, T_W):
+        return (K_20*theta**(T_W-20))
+
+
+test = VolumetricProcessDesignModel()
+print(test.K_T(0.678,1.06,18))
+
 
