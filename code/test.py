55fcf68a7d7eeff5528409237b4cec0fb048dcc6
Noah Davis <noah_davis@outlook.com>
2018-01-19 00:47:50 -0500

    All volumetric process models are finished (with tests) except for TSS Removal,Total Nitrogen Removal, and Fecal Coliform Removal

--- code/test.py
+++ code/test.py
@@ -14,11 +14,11 @@ class VolumetricProcessModelTest(unittest.TestCase):
         reedModel.BOD_Const['K_20'] = 0.678
         reedModel.BOD_Const['theta'] = 1.06
         t_w = 20  
-        test1 = reedModel.K_T(t_w)
+        test = reedModel.K_T('BOD',t_w)
 
-        self.assertEqual(test1, .678)
+        self.assertEqual(test, .678)
 
-    def testTreatmentArea(self):
+    def testTreatmentAreaBOD(self):
         reedModel = VolumetricProcessDesignModel()
         CEFONMA = Site()
         CEFONMA.flowRate = 18
@@ -31,10 +31,88 @@ class VolumetricProcessModelTest(unittest.TestCase):
         reedModel.BOD_Const['theta'] = 1.06
         t_w = 20
 
-        test2 = reedModel.treatmentArea(CEFONMA.flowRate, CEFONMA.waterQualityData['BOD'], effluentConcentration, reedModel.K_T(t_w))
+        test = reedModel.treatmentArea(CEFONMA.flowRate, CEFONMA.waterQualityData['BOD'], effluentConcentration, reedModel.K_T('BOD',t_w))
 
         #checks to 3 decimal places
-        self.assertAlmostEqual(test2, 642.857, 3)
+        self.assertAlmostEqual(test, 642.857, 3)
+
+    def testTreatmentAreaAmmonia(self):
+        reedModel = VolumetricProcessDesignModel()
+        CEFONMA = Site()
+        CEFONMA.flowRate = 18
+        CEFONMA.waterQualityData['ammonia'] = 32
+        effluentConcentration = 10.7213
+
+        reedModel.avgDepth = 0.4
+        reedModel.porosity = 0.7
+        reedModel.ammonia_Const['K_20'] = 0.2187
+        reedModel.ammonia_Const['theta'] = 1.048
+        t_w = 20
+
+        test = reedModel.treatmentArea(CEFONMA.flowRate, CEFONMA.waterQualityData['ammonia'], effluentConcentration, reedModel.K_T('ammonia',t_w))
+
+        #checks to 3 decimal places
+        self.assertAlmostEqual(test, 321.430, 3)
+
+    def testTreatmentAreaNitrate(self):
+        reedModel = VolumetricProcessDesignModel()
+        CEFONMA = Site()
+        CEFONMA.flowRate = 18
+        CEFONMA.waterQualityData['nitrate'] = 32
+        effluentConcentration = 0.215614
+
+        reedModel.avgDepth = 0.4
+        reedModel.porosity = 0.7
+        reedModel.ammonia_Const['K_20'] = 1
+        reedModel.ammonia_Const['theta'] = 1.15
+        t_w = 20
+
+        test = reedModel.treatmentArea(CEFONMA.flowRate, CEFONMA.waterQualityData['nitrate'], effluentConcentration, reedModel.K_T('nitrate',t_w))
+
+        #checks to 3 decimal places
+        self.assertAlmostEqual(test, 321.429, 3)
+
+    def testBODEffluent(self):
+        reedModel = VolumetricProcessDesignModel()
+        CEFONMA = Site()
+        CEFONMA.waterQualityData['BOD'] = 1625
+        t = 8
+
+        reedModel.BOD_Const['K_20'] = 0.678
+        reedModel.BOD_Const['theta'] = 1.06
+        t_w = 20
+
+        test = reedModel.effluent(CEFONMA.waterQualityData['BOD'], reedModel.K_T('BOD', t_w), t)
+        #checks to 3 decimal places
+        self.assertAlmostEqual(test, 7.165, 3)
+
+    def testNitrateEffluent(self):
+        reedModel = VolumetricProcessDesignModel()
+        CEFONMA = Site()
+        CEFONMA.waterQualityData['nitrate'] = 32
+        t = 5
+
+        reedModel.nitrate_Const['K_20'] = 1
+        reedModel.nitrate_Const['theta'] = 1.15
+        t_w = 20
+
+        test = reedModel.effluent(CEFONMA.waterQualityData['nitrate'], reedModel.K_T('nitrate', t_w), t)
+        #checks to 3 decimal places
+        self.assertAlmostEqual(test, 0.215614, 3)
+
+    def testPhosphorusEffluent(self):
+        reedModel = VolumetricProcessDesignModel()
+        CEFONMA = Site()
+        CEFONMA.flowRate = 18
+        CEFONMA.waterQualityData['Phosphorus'] = 7.76
+        
+        K_P = 2.73
+        HLR = 5
+        test = reedModel.phosphorusEffluent(CEFONMA.waterQualityData['Phosphorus'], K_P, HLR)
+
+        #checks to 3 decimal places
+        self.assertAlmostEqual(test, 4.49507, 3)
+
 
 
 if __name__ == '__main__':
--- code/wetlandCalc.py
+++ code/wetlandCalc.py
@@ -11,6 +11,8 @@ class VolumetricProcessDesignModel:
     def __init__(self):
         #From "Natural Wastewater Treatment Systems". First order removal model
         self.BOD_Const = {'K_20':0.678, 'theta':1.06}
+        self.ammonia_Const = {'K_20':0.2187, 'theta':1.048}
+        self.nitrate_Const = {'K_20':1, 'theta':1.15}
 
         #self.effluentConcentration = 10
 
@@ -22,11 +24,28 @@ class VolumetricProcessDesignModel:
 
 
     #Volumetric Design Equations
-    def K_T(self, T_W):
-        return (self.BOD_Const['K_20']*self.BOD_Const['theta']**(T_W-20))
+    #BOD
+    def K_T(self, qualityType, T_W):
+        if qualityType == 'BOD':
+            K_20 = self.BOD_Const['K_20']
+            theta = self.BOD_Const['theta']
+        elif qualityType == 'ammonia':
+            K_20 = self.ammonia_Const['K_20']
+            theta = self.ammonia_Const['theta']
+        elif qualityType == 'nitrate':
+            K_20 = self.nitrate_Const['K_20']
+            theta = self.nitrate_Const['theta']
+
+        return (K_20*theta**(T_W-20))
 
     def treatmentArea(self, avgFlowRate,influentConcentration, effluentConcentration, K_T):
         return avgFlowRate*((math.log(influentConcentration/effluentConcentration))/(K_T*self.avgDepth*self.porosity))
+
+    def effluent(self, influentConcentration, K_T, t):
+        return influentConcentration*math.exp(-K_T*t)
+
+    def phosphorusEffluent(self, influentConcentration, K_P, HLR):
+        return influentConcentration*math.exp(-K_P/HLR)
 
 """
 
