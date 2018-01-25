7c243dc5c72601e1c1405ab4e8a9b21af0e7865c
Noah Davis <noah_davis@outlook.com>
2018-01-25 11:54:37 -0500

    Added Kadlec models

--- code/main.py
+++ code/main.py
@@ -3,15 +3,18 @@ import sys
 
 #importing class definitions
 from siteInfo import Site
-from wetlandCalc import ReedSubsurfaceFlow, ReedFreewaterFlow
+from wetlandCalc import ReedSubsurfaceFlow, ReedFreewaterFlow, KadlecSubsurfaceFlow
 
 def main():
 
     CEFONMA = Site()    
     Reed = ReedSubsurfaceFlow()
+    Kadlec = KadlecSubsurfaceFlow()
    
     print(CEFONMA.flowRate)
     print("Area needed (with BOD = 155): %d m^2" % Reed.treatmentArea(CEFONMA.flowRate, 155, 10, Reed.K_T('BOD', 18)))
+
+    print("Area needed (with BOD = 155): %d m^2" % Kadlec.treatmentArea('BOD', CEFONMA.flowRate, 286, 10, 18))
 
     print("Area needed (with BOD = 286): %d m^2" %Reed.treatmentArea(CEFONMA.flowRate, 286, 10, Reed.K_T('BOD', 18)))
 
--- code/wetlandCalc.py
+++ code/wetlandCalc.py
@@ -19,10 +19,6 @@ class ReedModel:
         elif qualityType == 'nitrate':
             K_20 = self.nitrate_Const['K_20']
             theta = self.nitrate_Const['theta']
-        elif qualityType == 'coliform':
-            K_20 = self.coliform_Const['K_20']
-            theta = self.coliform_Const['theta']
-
 
         return (K_20*theta**(T_W-20))
 
@@ -38,10 +34,7 @@ class ReedModel:
     def fecalColiformRemoval(self, influentConcentration, K_P, HRT, numberOfCells):
         return influentConcentration(1/(1+K_P*HRT)**numberOfCells)
 
-    def kadlecModel(self, avgFlowRate, influentConcentration, effluentConcentration,  backgroundConcentration, K_T):
-        return avgFlowRate*(math.log((influentConcentration - backgroundConcentration)/(effluentConcentration - backgroundConcentration))/(K_T))
-        #return math.log((effluentConcentration - backgroundConcentration)/(influentConcentration - backgroundConcentration))
-
+    
 class ReedSubsurfaceFlow(ReedModel):
 
     def __init__(self):
@@ -81,4 +74,67 @@ class ReedFreewaterFlow(ReedModel):
         return influentConcentration*(0.11139+(0.00213*HLR))
 
 
+#Kadlec Models
 
+class Kadlec():
+
+    #Volumetric Design Equations
+    #BOD
+    def K_T(self, qualityType, T_W):
+        if qualityType == 'BOD':
+            K_20 = self.BOD_Const['K_20']
+            theta = self.BOD_Const['theta']
+        elif qualityType == 'ammonia':
+            K_20 = self.ammonia_Const['K_20']
+            theta = self.ammonia_Const['theta']
+        elif qualityType == 'nitrate':
+            K_20 = self.nitrate_Const['K_20']
+            theta = self.nitrate_Const['theta']
+        elif qualityType == 'coliform':
+            K_20 = self.coliform_Const['K_20']
+            theta = self.coliform_Const['theta']
+
+        return (K_20*theta**(T_W-20))
+
+
+    def treatmentArea(self, qualityType, avgFlowRate,influentConcentration, effluentConcentration, K_Tinput):
+        if qualityType == 'BOD':
+            backgroundConcentration = self.BOD_Const['backgroundConcentration']
+        elif qualityType == 'ammonia':
+            backgroundConcentration = self.ammonia_Const['backgroundConcentration']
+        elif qualityType == 'nitrate':
+            backgroundConcentration = self.nitrate_Const['backgroundConcentration']
+        elif qualityType == 'organicNitrogen':
+            backgroundConcentration = self.organicNitrogen_Const['backgroundConcentration']
+        elif qualityType == 'totalNitrogen':
+            backgroundConcentration = self.totalNitrogen_Const['backgroundConcentration']
+        elif qualityType == 'toalPhosphorus':
+            backgroundConcentration = self.totalPhosphorus_Const['backgroundConcentration']
+        elif qualityType == 'coliform':
+            backgroundConcentration = self.coliform_Const['backgroundConcentration']
+
+
+        return avgFlowRate*(math.log((influentConcentration - backgroundConcentration)/(effluentConcentration - backgroundConcentration))/(self.K_T(qualityType, K_Tinput)))
+
+
+class KadlecSubsurfaceFlow(Kadlec):
+    def __init__(self):
+        #From "Natural Wastewater Treatment Systems". First order removal model
+        self.BOD_Const = {'K_20':0.3205, 'theta':1.057, 'backgroundConcentration':3.0}
+        self.TSS_Const = {'k_20':0.1186, 'theta':1.00, 'backgroundConcentration':6.0}
+        self.organicNitrogen_Const = {'k_20':0.0959, 'theta':1.05, 'backgroundConcentration':1.5}
+        self.ammonia_Const = {'K_20':0.0932, 'theta':1.05, 'backgroundConcentration':0}
+        # For Ammonia: The KNH value would be 0.4107 with a fully developed root zone and 0.01854
+        self.nitrate_Const = {'K_20':0.1370, 'theta':1.05, 'backgroundConcentration':0} #^From Reed
+        self.totalNitrogen_Const = {'K_20':0.0274, 'theta':1.05, 'backgroundConcentration':1.5}
+        self.totalPhospohorus_Const = {'K_20':0.0249, 'theta':1.097, 'backgroundConcentration':0}
+        self.coliform_Const = {'K_20':0.274, 'theta':1.03, 'backgroundConcentration':200} #From kadlec
+
+        #initialize with reasonable value used in example, but should change this
+        self.avgDepth = 0.5
+
+        #initialize to an average value from Reed book
+        self.porosity = 0.8
+
+
+  
